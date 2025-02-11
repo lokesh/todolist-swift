34efc2c1567ae45fa4f9622b95f1114e1ed741b4
Lokesh Dhakar <lokesh.dhakar@gmail.com>
2025-02-11 15:30:17 -0800

    Add authentication state management and view routing

--- TodoList/ViewModels/MainViewModel.swift
+++ TodoList/ViewModels/MainViewModel.swift
@@ -5,4 +5,20 @@
 //  Created by Lokesh Dhakar on 2/11/25.
 //
 
+import FirebaseAuth
 import Foundation
+
+class MainViewModel: ObservableObject {
+  @Published var currentUserId: String = ""
+  private var handler: AuthStateDidChangeListenerHandle?
+
+  init() {
+    self.handler = Auth.auth().addStateDidChangeListener { [weak self] _, user in
+      self?.currentUserId = user?.uid ?? ""
+    }
+  }
+
+  public var isSignedIn: Bool {
+    return Auth.auth().currentUser != nil
+  }
+} 
